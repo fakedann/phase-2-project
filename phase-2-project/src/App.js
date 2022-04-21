def54027215f95880c68f2f506a5bcbf7a3385a9
Daniel Escalona <daniel07escalona@gmail.com>
2022-04-21 12:54:29 -0400

    Card model has been added. More tweaks are needed

--- phase-2-project/src/App.js
+++ phase-2-project/src/App.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Route, Switch } from "react-router-dom";
 import BookSelection from './BookSelection';
 import ReviewForm from './ReviewForm';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
 
 function App() {
   return (
@@ -11,6 +14,22 @@ function App() {
       <Switch>
         <Route exact path="/">
           <p>Home page</p>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={require('./book.jpeg')} alt="Card Image"/>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick ple text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+          </Card>
+          
         </Route>
         <Route exact path="/book-selection">
           <BookSelection />
