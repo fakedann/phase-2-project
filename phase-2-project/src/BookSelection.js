c6822e1575f783addcd57fa72c4a7b9e02887780
Daniel Escalona <daniel07escalona@gmail.com>
2022-05-04 13:42:07 -0400

    Add popovers in order to connect books to database

--- phase-2-project/src/BookSelection.js
+++ phase-2-project/src/BookSelection.js
@@ -7,6 +7,11 @@ import ModalBody from 'react-bootstrap/ModalBody'
 import ModalFooter from 'react-bootstrap/ModalFooter'
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
+import Overlay from 'react-bootstrap/Overlay'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Popover from 'react-bootstrap/Popover'
+import PopoverBody from 'react-bootstrap/PopoverBody'
+import PopoverHeader from 'react-bootstrap/PopoverHeader'
 
 function BookSelection(){
 
@@ -14,13 +19,38 @@ function BookSelection(){
   const [checkedStatus, setChecked] = useState('date')
   const [results, setResults] = useState([])
   // const displayedCards = results.map( bookObj => <CardItem key={bookObj.title} book={bookObj}/>)
-  const tableResults = results?.map( (tableObj, index) => <tr key={index}><td>{index}</td><td>{tableObj.author}</td><td>{tableObj.title}</td><td>{tableObj.publisher}</td><td>{tableObj.description}</td></tr>)
 
   const [show, setShow] = useState(false);
   const [condModal, setModal] = useState(true)
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [btnSrch, setBtn] = useState('')
+  const [btnStates, setStates] = useState([])
+  console.log(btnStates)
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Tell us more about how you feel:</Popover.Header>
+      <Popover.Body>
+        <button className="myBtns" onClick={handleBtnClick} variant="warning">Have Read</button>
+        <button className="myBtns" onClick={handleBtnClick}  variant="warning">Will Read</button>
+        <button className="myBtns" onClick={handleBtnClick} >Liked</button>
+        <button className="myBtns" onClick={handleBtnClick} >Disliked</button>
+        <form onSubmit={handleBtnSubmit}>
+            <label htmlFor="search">Enter Your Name:</label>
+            <input type="text" value={btnSrch} placeholder="Daniel Escalona" onChange={e => setBtn(e.target.value)}/>
+            <button type="submit">Submit</button>
+        </form>
+      </Popover.Body>
+    </Popover>
+  );
+
+
+  const tableResults = results.map( (tableObj, index) => <tr key={index}><td>{index}</td><td>{tableObj.author}</td><td>{tableObj.title}</td><td>{tableObj.publisher}</td><td>{tableObj.description}</td><td><OverlayTrigger trigger="click" placement="right" overlay={popover}>
+  <Button variant="success">Add to database</Button>
+</OverlayTrigger></td></tr>)
 
   const table = <Table striped bordered hover>
   <thead>
@@ -36,6 +66,24 @@ function BookSelection(){
     {tableResults}
   </tbody>
 </Table>
+
+function handleBtnClick(e) {
+
+  
+  if(e.target.className === "myBtns"){
+    e.target.className = "btnClicked"
+    setStates([...btnStates, e.target.innerText])
+  }else{
+    e.target.className = "myBtns"
+    setStates( btnStates.filter( btnObj => btnObj !== e.target.innerText))
+  }
+  console.log(e.target.innerText)
+}
+
+function handleBtnSubmit(e) {
+  e.preventDefault()
+  console.log('submitted')
+}
   
 
 
